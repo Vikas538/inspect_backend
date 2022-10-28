1c50d1e7ff8161d330f844c040f53124e8214a55
vikas <vikas.reddy@kenko-health.in>
2022-10-28 22:55:12 +0530

    added db schema and all modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderManagementModule } from './modules/order-management/order-management.module';
+import { ControllerModule } from './controller/controller.module';
+import { LoginModule } from './modules/login/login.module';
+import { DaoModule } from './dao/dao.module';
 
 @Module({
-  imports: [],
+  imports: [OrderManagementModule, ControllerModule, LoginModule, DaoModule],
   controllers: [AppController],
   providers: [AppService],
 })
