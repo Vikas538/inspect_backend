15781d6bfbfa7f222f9eb1eaee431f98acf451a6
vikas <vikas.reddy@kenko-health.in>
2022-12-10 04:56:23 +0530

    added db schema

---
+++ src/common/common.crypt.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { createHash } from 'crypto';
+
+@Injectable()
+export class CryptService {
+
+
+  hash(plainText: string) {
+    
+    const hash = createHash('sha256').update(plainText).digest('hex')
+
+    return hash
+    }
+    
+}
--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { CryptService } from './common.crypt';
+import { CommonService } from './common.service';
 
-@Module({})
-export class CommonModule {}
+@Module({
+    providers: [
+        CryptService,
+        CommonService
+    ],
+    exports: [  
+        CryptService,
+        CommonService
+        
+    ],
+  })
+export class CommonModule {
+
+
+}
