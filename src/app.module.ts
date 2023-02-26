1ac469f2b9d6adab7242c69659e5f872891e56ee
vikas1440 <vikas.reddy@kenko-health.in>
2023-02-26 08:33:24 +0530

    added default mongourl

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,19 +12,17 @@ import { DaoModule } from './dao/dao.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CtxIdMiddleware } from './common/middlewares/ctxId.middleware';
 import { MongooseModule } from '@nestjs/mongoose';
-
-const configService = new ConfigService();
-console.log('=======================>,', configService.get('DATABASE_URL'));
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb://mongoadmin:secret@localhost:27017/inspect?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false',
+      'mongodb://root:root@localhost:27017/nestjs-tutorial?authSource=admin&readPreference=primary',
     ),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     OrderManagementModule,
     AuthModule,
+    DaoModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -38,8 +38,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.setGlobalPrefix('/api/v1');
-  console.log('========================>Port', configService.get('PORT'));
-  await app.listen(configService.get('PORT'));
+  await app.listen(3002);
 }
 
 bootstrap();
